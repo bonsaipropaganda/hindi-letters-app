37aed0e97f865eb9180d3d1ee6df357ac0db4a51
Seth Louis <sethvihanlouis@gmail.com>
2023-10-09 04:10:18 -0400

    started adding audio

--- sound_manager.gd
+++ sound_manager.gd
@@ -1,9 +1,54 @@
 extends Node2D
 
-@onready var क = $"क"
+@onready var audio_stream_player_1 = $AudioStreamPlayer2D
+@onready var audio_stream_player_2 = $AudioStreamPlayer2D2
+@onready var audio_stream_player_3 = $AudioStreamPlayer2D3
+@onready var audio_stream_player_4 = $AudioStreamPlayer2D4
+@onready var audio_stream_player_5 = $AudioStreamPlayer2D5
+@onready var audio_stream_player_6 = $AudioStreamPlayer2D6
+@onready var audio_stream_player_7 = $AudioStreamPlayer2D7
+@onready var audio_stream_player_8 = $AudioStreamPlayer2D8
+@onready var audio_stream_player_9 = $AudioStreamPlayer2D9
+@onready var audio_stream_player_10 = $AudioStreamPlayer2D10
+@onready var audio_stream_player_11 = $AudioStreamPlayer2D11
+@onready var audio_stream_player_12 = $AudioStreamPlayer2D12
+@onready var audio_stream_player_13 = $AudioStreamPlayer2D13
+@onready var audio_stream_player_14 = $AudioStreamPlayer2D14
+@onready var audio_stream_player_15 = $AudioStreamPlayer2D15
+@onready var audio_stream_player_16 = $AudioStreamPlayer2D16
+@onready var audio_stream_player_17 = $AudioStreamPlayer2D17
+@onready var audio_stream_player_18 = $AudioStreamPlayer2D18
+@onready var audio_stream_player_19 = $AudioStreamPlayer2D19
+@onready var audio_stream_player_20 = $AudioStreamPlayer2D20
+@onready var audio_stream_player_21 = $AudioStreamPlayer2D21
+@onready var audio_stream_player_22 = $AudioStreamPlayer2D22
+@onready var audio_stream_player_23 = $AudioStreamPlayer2D23
+@onready var audio_stream_player_24 = $AudioStreamPlayer2D24
+@onready var audio_stream_player_25 = $AudioStreamPlayer2D25
+@onready var audio_stream_player_26 = $AudioStreamPlayer2D26
+@onready var audio_stream_player_27 = $AudioStreamPlayer2D27
+@onready var audio_stream_player_28 = $AudioStreamPlayer2D28
+@onready var audio_stream_player_29 = $AudioStreamPlayer2D29
+@onready var audio_stream_player_30 = $AudioStreamPlayer2D30
+@onready var audio_stream_player_31 = $AudioStreamPlayer2D31
+@onready var audio_stream_player_32 = $AudioStreamPlayer2D32
+@onready var audio_stream_player_33 = $AudioStreamPlayer2D33
+@onready var audio_stream_player_34 = $AudioStreamPlayer2D34
+@onready var audio_stream_player_35 = $AudioStreamPlayer2D35
+@onready var audio_stream_player_36 = $AudioStreamPlayer2D36
+@onready var audio_stream_player_37 = $AudioStreamPlayer2D37
+@onready var audio_stream_player_38 = $AudioStreamPlayer2D38
+@onready var audio_stream_player_39 = $AudioStreamPlayer2D39
+@onready var audio_stream_player_40 = $AudioStreamPlayer2D40
+@onready var audio_stream_player_41 = $AudioStreamPlayer2D41
+@onready var audio_stream_player_42 = $AudioStreamPlayer2D42
+@onready var audio_stream_player_43 = $AudioStreamPlayer2D43
+@onready var audio_stream_player_44 = $AudioStreamPlayer2D44
+@onready var audio_stream_player_45 = $AudioStreamPlayer2D45
+@onready var audio_stream_player_46 = $AudioStreamPlayer2D46
 
 
 
 
-func _on_कbutton_pressed():
-	क.play()
+func _on_अbutton_pressed():
+	audio_stream_player_1.play()
